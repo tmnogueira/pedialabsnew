0c981b4c32d68d23cf12ea3d9a03a8da19732f1a
Zarina Mustapha <zarina@columbia.edu>
2014-01-22 12:58:15 -0500

    theming

--- pedialabsnew/urls.py
+++ pedialabsnew/urls.py
@@ -18,12 +18,12 @@ if hasattr(settings, 'WIND_BASE'):
     logout_page = (
         r'^accounts/logout/$',
         'djangowind.views.logout',
-        {'next_page': redirect_after_logout})
+        {'next_page': '/'})  # redirect_after_logout})
 
 urlpatterns = patterns(
     '',
     auth_urls,
-    logout_page,
+    #logout_page,
     (r'^registration/', include('registration.backends.default.urls')),
     (r'^$', 'pedialabsnew.main.views.index'),
     (r'^admin/', include(admin.site.urls)),
@@ -32,6 +32,9 @@ urlpatterns = patterns(
     (r'smoketest/', include('smoketest.urls')),
     (r'^uploads/(?P<path>.*)$',
      'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    (r'^logout/$',
+     'django.contrib.auth.views.logout',
+     {'next_page': '/'}),
     (r'^pagetree/', include('pagetree.urls')),
     (r'^quizblock/', include('quizblock.urls')),
     (r'^pages/edit/(?P<path>.*)$', 'pedialabsnew.main.views.edit_page',
