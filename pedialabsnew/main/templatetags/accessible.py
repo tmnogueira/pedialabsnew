fae41de5abc88886430d7c26db63ab4c1ff69def
Anders Pearson <anders@columbia.edu>
2014-02-11 11:52:18 -0500

    accessible templatetag was missing block_submitted function. PMT #91165

--- pedialabsnew/main/templatetags/accessible.py
+++ pedialabsnew/main/templatetags/accessible.py
@@ -3,33 +3,19 @@ from django import template
 register = template.Library()
 
 
-class AccessibleNode(template.Node):
-    def __init__(self, section, nodelist_true, nodelist_false=None):
-        self.nodelist_true = nodelist_true
-        self.nodelist_false = nodelist_false
-        self.section = section
-
-    def render(self, context):
-        s = context[self.section]
-        r = context['request']
-        u = r.user
-        if section.gate_check(u[0]) and section.submitted(u):
-            return self.nodelist_true.render(context)
-        else:
-            return self.nodelist_false.render(context)
-
-
-@register.tag('ifaccessible')
-def accessible(parser, token):
-    section = token.split_contents()[1:][0]
-    nodelist_true = parser.parse(('else', 'endifaccessible'))
-    token = parser.next_token()
-    if token.contents == 'else':
-        nodelist_false = parser.parse(('endifaccessible',))
-        parser.delete_first_token()
-    else:
-        nodelist_false = None
-    return AccessibleNode(section, nodelist_true, nodelist_false)
+def block_submitted(block, user):
+    if user.is_anonymous():
+        # anon can't have submitted a block
+        return False
+    if hasattr(block, 'needs_submit'):
+        if block.needs_submit():
+            try:
+                s = block.unlocked(user)
+                if not s:
+                    return False
+            except:
+                pass
+    return True
 
 
 class SubmittedNode(template.Node):
