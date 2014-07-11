eea29f0eb32c7ba87bdb15175ad591fa6a1044e9
Zarina Mustapha <zarina@columbia.edu>
2014-07-11 15:54:45 -0400

    sidemenu almost done

--- media/js/sidemenu.js
+++ media/js/sidemenu.js
@@ -2,37 +2,38 @@ jQuery(document).ready(function() {
   if(!$('.labs-sectionmenu').length) return false;
 
   var elt = jQuery('.menu-active');
-  var eltIdx = jQuery(elt).attr('data-depth');
+  var currentIdx = parseInt(jQuery(elt).attr('data-depth'), 10);
   
   function initMenu() {
     jQuery(elt).show();
   }
   
-  function showParent() {
-    for (idx = eltIdx-1; idx >= 3; idx = idx-1) {
-      var parentClassIdx = '.menu-'+ idx;
-      var parent = jQuery(elt).prevAll(parentClassIdx).first();
-      parent.show();
-    }
-  }
-  
-  function showSibling() {
+  function showSibling(eltIdx) {
     var prev = eltIdx-1;
-    prevSibling = jQuery(elt).prevUntil('.menu-'+prev, '.menu-'+eltIdx);
-    nextSibling = jQuery(elt).nextUntil('.menu-'+prev, '.menu-'+eltIdx);
+    var prevSibling = jQuery(elt).prevUntil('.menu-'+prev, '.menu-'+eltIdx);
+    var nextSibling = jQuery(elt).nextUntil('.menu-'+prev, '.menu-'+eltIdx);
     prevSibling.show();
     nextSibling.show();
   }
   
-  function showChildren() {
+  function showParent(eltIdx) {
+    for (idx = eltIdx-1; idx >= 3; idx = idx-1) {
+      var parentClassIdx = '.menu-'+ idx;
+      var parent = jQuery(elt).prevAll(parentClassIdx).first();
+      parent.show();
+      showSibling(idx);
+    }
+  }
+  
+  function showChildren(eltIdx) {
     var next = eltIdx+1;
-    var children = jQuery(elt).nextUntil('.menu-'+next, '.menu-'+eltIdx);
+    var children = jQuery(elt).nextUntil('.menu-'+eltIdx, '.menu-'+next);
     children.show()
   }
   
   initMenu();
-  showParent();
-  showChildren();
-  showSibling();
+  showParent(currentIdx);
+  showChildren(currentIdx);
+  showSibling(currentIdx);
  
 });
