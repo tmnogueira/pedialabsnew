1f951baba9a93619e7e366e6682fbdc9681fa76c
zarina <zarina@columbia.edu>
2016-01-12 15:13:04 -0500

    all kinds of migration

--- pedialabsnew/rstplot/migrations/0001_initial.py
+++ pedialabsnew/rstplot/migrations/0001_initial.py
@@ -1,31 +1,23 @@
-# -*- coding: utf-8 -*-
 # flake8: noqa
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'RstPlotBlock'
-        db.create_table(u'rstplot_rstplotblock', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-        ))
-        db.send_create_signal(u'rstplot', ['RstPlotBlock'])
+    dependencies = [
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'RstPlotBlock'
-        db.delete_table(u'rstplot_rstplotblock')
-
-
-    models = {
-        u'rstplot.rstplotblock': {
-            'Meta': {'object_name': 'RstPlotBlock'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
-        }
-    }
-
-    complete_apps = ['rstplot']
+    operations = [
+        migrations.CreateModel(
+            name='RstPlotBlock',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
