8dbf25552d25d8940a6eb49b6f0d1b841bb39429
Susan Dreher <sdreher@columbia.edu>
2016-01-14 15:57:43 -0500

    Makefile refactor * and jscs/jshint

--- media/js/sidemenu.js
+++ media/js/sidemenu.js
@@ -1,46 +1,54 @@
 jQuery(document).ready(function() {
-  if(!$('.sectionmenu').length) return false;
-
-  var elt = jQuery('.menu-active');
-  var currentIdx = parseInt(jQuery(elt).attr('data-depth'), 10);
-
-  jQuery('.menu-item').last().addClass('menu-last');
-
-  if(currentIdx == 2) return false;
-
-  function initMenu() {
-    jQuery(elt).show();
-  }
-  
-  function showSibling(eltIdx) {
-    var prev = eltIdx-1;
-    var prevSibling = jQuery(elt).prevUntil('.menu-'+prev, '.menu-'+eltIdx);
-    var nextSibling = jQuery(elt).nextUntil('.menu-'+prev, '.menu-'+eltIdx);
-    prevSibling.show();
-    nextSibling.show();
-  }
-  
-  function showParent(eltIdx) {
-    for (idx = eltIdx-1; idx >= 3; idx = idx-1) {
-      var parentClassIdx = '.menu-'+ idx;
-      var parent = jQuery(elt).prevAll(parentClassIdx).first();
-      parent.show();
-      if (idx == 3) {
-        jQuery(elt).prevAll(parentClassIdx).first().addClass('toggle-open');
-      }
-      showSibling(idx);
+    if (!$('.sectionmenu').length) {
+        return false;
     }
-  }
-  
-  function showChildren(eltIdx) {
-    var next = eltIdx+1;
-    var children = jQuery(elt).nextUntil('.menu-'+eltIdx, '.menu-'+next);
-    children.show()
-  }
-  
-  initMenu();
-  showParent(currentIdx);
-  showChildren(currentIdx);
-  showSibling(currentIdx);
- 
+
+    var elt = jQuery('.menu-active');
+    var currentIdx = parseInt(jQuery(elt).attr('data-depth'), 10);
+
+    jQuery('.menu-item').last().addClass('menu-last');
+
+    if (currentIdx === 2) {
+        return false;
+    }
+
+    function initMenu() {
+        jQuery(elt).show();
+    }
+
+    function showSibling(eltIdx) {
+        var prev = eltIdx - 1;
+        var prevSibling = jQuery(elt).prevUntil('.menu-' + prev,
+                                                '.menu-' + eltIdx);
+        var nextSibling = jQuery(elt).nextUntil('.menu-' + prev,
+                                                '.menu-' + eltIdx);
+        prevSibling.show();
+        nextSibling.show();
+    }
+
+    function showParent(eltIdx) {
+        for (var idx = eltIdx - 1; idx >= 3; idx = idx - 1) {
+            var parentClassIdx = '.menu-' + idx;
+            var parent = jQuery(elt).prevAll(parentClassIdx).first();
+            parent.show();
+            if (idx == 3) {
+                jQuery(elt).prevAll(parentClassIdx)
+                           .first()
+                           .addClass('toggle-open');
+            }
+            showSibling(idx);
+        }
+    }
+
+    function showChildren(eltIdx) {
+        var next = eltIdx + 1;
+        var children = jQuery(elt).nextUntil('.menu-' + eltIdx,
+                                             '.menu-' + next);
+        children.show();
+    }
+
+    initMenu();
+    showParent(currentIdx);
+    showChildren(currentIdx);
+    showSibling(currentIdx);
 });
--- media/stick.js
+++ media/stick.js
@@ -1,5 +1,6 @@
+/* global exports */
 (function(exports) {
-"use strict";
+'use strict';
 /*
  * A simpler boomerang: https://github.com/yahoo/boomerang that just
  * does navigation timing. Requires jquery.
@@ -12,21 +13,25 @@ exports.send = function(url) {
     if (perf) {
         setTimeout(function() {
             $.post(url,  {
-                'window.performance.timing.navigationStart': perf.timing.navigationStart,
-                'window.performance.timing.domComplete': perf.timing.domComplete,
-                'window.performance.timing.domInteractive': perf.timing.domInteractive,
-                'window.performance.timing.domLoading': perf.timing.domLoading,
-                'window.performance.timing.loadEventEnd': perf.timing.loadEventEnd,
-                'window.performance.timing.responseStart': perf.timing.responseStart,
-                'window.performance.navigation.redirectCount': perf.navigation.redirectCount,
-                'window.performance.navigation.type': perf.navigation.type,
+                'window.performance.timing.navigationStart':
+                    perf.timing.navigationStart,
+                'window.performance.timing.domComplete':
+                    perf.timing.domComplete,
+                'window.performance.timing.domInteractive':
+                    perf.timing.domInteractive,
+                'window.performance.timing.domLoading':
+                    perf.timing.domLoading,
+                'window.performance.timing.loadEventEnd':
+                    perf.timing.loadEventEnd,
+                'window.performance.timing.responseStart':
+                    perf.timing.responseStart,
+                'window.performance.navigation.redirectCount':
+                    perf.navigation.redirectCount,
+                'window.performance.navigation.type':
+                    perf.navigation.type,
                 'client': 'stick'
             });
         }, 1000);
     }
 };
-
 })(typeof exports === 'undefined' ? (this.stick = {}) : exports);
-
-
-
